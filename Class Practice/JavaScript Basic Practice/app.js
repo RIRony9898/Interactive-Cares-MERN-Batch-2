982ceeafa6f2dd5b59c4e5e63d9a95a2bda7a006
Rony <rirony59@gmail.com>
2023-10-26 16:38:56 +0600

    js start

--- Class Practice/JavaScript Basic Practice/app.js
+++ Class Practice/JavaScript Basic Practice/app.js
@@ -1,2 +1,45 @@
-// class - 1.2
-console.log(Hello);
+//class - 1.2 Basis JS structure and how to show js output
+console.log("Hello World");
+
+//class - 1.3 variable & constant in js
+
+//Variable neyar jonno let & var use kora hoy
+let firstName = 'John' //output: John
+var lastName = 'Islam'
+var age = '26'
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+//variable jekono somoy change kora jay
+firstName = 'Raisul'
+console.log(firstName); //output: Raisul
+
+//constant neyar jonno const use kora hoy
+const PI = 3.1416
+console.log(PI);
+
+//class - 1.4 Naming a variable
+
+/*Naming conversion er jonno 4 ta way ase
+1. firstName -This is mostly use - etar nam hocche camelCase
+2. first_Name -eta php te besi use kora hoy
+3. FirstName -class er nam er khetre erkm vabe lekha hoy
+4. FIRSTNAME -constant lkhar khetre eta use kora hoy*/
+const TITLE = 'I live in Bangladesh'
+console.log(TITLE);
+
+//Varaible er khetre ki ki lkha jay - a-z,_,$.0-9
+
+//class - 1.5 Comments in JS
+//Single Line Comment
+/*Multiple
+Line
+Comment*/
+
+
+
+
+
+
+
