209860e9fd5915ba3e8b936203f44c87631210c9
Rony <rirony59@gmail.com>
2023-12-20 02:49:02 +0600

    Added stylesheet and event listener to increment a number and display it.

---
+++ LWS React in YT/Class 1/Practice 0 by using React/script.js
@@ -0,0 +1,32 @@
+const domContainer = document.querySelector("#root");
+
+const Increment = () => {
+  const [counter, setCounter] = React.useState(0);
+  return (
+    
+      <div>
+      <h1 id="display">{ counter }</h1>
+        <div>
+          <button id="button" onClick = { () => setCounter(counter + 1)}>Increment +</button>
+        </div>
+      </div>
+    
+  )
+}
+
+ReactDOM.render(
+  <div class="container">
+    <Increment />
+    <Increment />
+    <Increment />
+  </div>,
+  domContainer);
+
+// let number = 0;
+// const display = document.querySelector("#display");
+// const button = document.querySelector("#button");
+
+// button.addEventListener("click", () => {
+//   number++;
+//   display.textContent = number;
+// });
